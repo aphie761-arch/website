6363660f3ff87a3cc497da65a91d10345c0479b6
aphie761-arch <aphie761@gmail.com>
2026-06-17 16:35:44 +0300

    Updated branding to Entebbe Flying School LTD

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/courses')
+def courses():
+    return render_template('courses.html')
+
+@app.route('/admissions')
+def admissions():
+    return render_template('admissions.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/gallery')
+def gallery():
+    return render_template('gallery.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
